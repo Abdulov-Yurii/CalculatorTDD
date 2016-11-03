19a47660585dab213e5d70f41abcbc7b0def2439
Yurik Abdulayev <yuriia@softwareplanet.com>
2016-11-03 16:06:02 +0200

    5: Allow the Add method to handle new lines between numbers (instead of commas).

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,7 +4,7 @@
 public class StringCalculator {
     static int add(String numbers) {
         int resultValue = 0;
-        String[] numbersArray = numbers.split(",");
+        String[] numbersArray = numbers.split("[,n]");
         for (String number : numbersArray) {
             if (!number.isEmpty()) {
                 resultValue += Integer.parseInt(number);
